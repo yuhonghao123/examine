003dc292c1e091a9ba3bd9b1998525b46f54f69d
yuhonghao0707 <1448856087@qq.com>
2020-05-24 19:34:24 +0800

    Initial commit

---
+++ src/test/java/com/example/examine/ErciyuanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErciyuanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
